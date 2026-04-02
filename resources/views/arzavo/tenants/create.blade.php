8b8bb3b8cfd8d22c0061fe234ec70aa2b37e6f17
MRK <monisrazakhan2001@gmail.com>
2026-04-02 19:02:09 +0530

    fixed-11111-11-11-11-11

--- resources/views/arzavo/tenants/create.blade.php
+++ resources/views/arzavo/tenants/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('arzavo.tenants.store') }}" method="POST" class="space-y-4">
+<form action="{{ route('tenants.store') }}" method="POST" class="space-y-4">
     @csrf
     <input type="text" name="name" placeholder="Tenant Name" class="w-full border-primary border-rounded p-3 mb-4">
     <input type="text" name="subdomain" placeholder="Subdomain" class="w-full border-primary border-rounded p-3 mb-4">
