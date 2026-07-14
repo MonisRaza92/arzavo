42fd193310b869be9049cd0b0f6a766523b2f694
MRK <monisrazakhan2001@gmail.com>
2026-07-14 19:53:46 +0530

    clean: remove all data_* blocks and sections, and redundant slideshow assets from Lex theme

--- resources/views/tenant/themes/lex/blocks/data_card.blade.php
+++
@@ -1,6 +0,0 @@
-<div {!! $block->attributes() !!} class="overflow-hidden w-full arz-{{ $block->card_type }}" style="">
-    <div class="flex flex-col" style="gap:{{$block->block_gap}}px">
-        {{-- ✅ CARD CONTENT BLOCKS --}}
-        {!! $block->blocks()->render(['data' => $data]) !!}
-    </div>
-</div>
