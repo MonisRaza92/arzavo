ceb66f017737c3c77991af8133c96cc055a1316d
MRK <monisrazakhan2001@gmail.com>
2026-08-15 20:22:13 +0530

    fix: make navbar auth-aware with direct admin panel link and auto-detect secure session cookies

--- config/session.php
+++ config/session.php
@@ -180,7 +180,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', true),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
 
     /*
     |--------------------------------------------------------------------------
